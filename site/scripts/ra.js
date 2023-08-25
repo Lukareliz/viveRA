0fabf0cb89edfe4049c79d565741a8132fe01139
= <lyulucca@gmail.com>
2023-08-24 21:04:56 -0300

    Fix/Feat: Fixed Zoom bug and New Images added

--- site/scripts/ra.js
+++ site/scripts/ra.js
@@ -1,11 +1,11 @@
-const NUMBER_OF_IMAGES = 5;
+const NUMBER_OF_IMAGES = 44;
 
-function injectAScene(){
-    const imgNumber = Math.ceil(Math.random()*NUMBER_OF_IMAGES)
+function injectAScene() {
+    const imgNumber = Math.ceil(Math.random() * NUMBER_OF_IMAGES)
     const aSceneHTML = `
         <a-scene vr-mode-ui="enabled: false" embedded arjs="debugUIEnabled: false;">
             <a-assets>
-                <img src="/assets/img${imgNumber.toString().padStart(2,"0")}.png" alt="" id="logo">
+                <img src="./assets/img${imgNumber.toString().padStart(2, "0")}.png" alt="" id="logo">
             </a-assets>
             <a-marker preset="hiro">
                 <a-image width="1" height="1" rotation="-90 0 0" position="0 0 0" src="#logo"></a-image>
@@ -13,5 +13,6 @@ function injectAScene(){
             <a-entity camera></a-entity>
         </a-scene>
     `
-    document.body.innerHTML+=aSceneHTML
+    document.body.innerHTML += aSceneHTML
 }
+
